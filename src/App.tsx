d5c413382fabba44c216573ac30dc8a9ffacff94
gonzaloandrade217 <gonzaloandrade217@gmail.com>
2025-03-27 17:27:39 -0300

    Mi primer commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import react from 'react';
+import axios from 'axios';
+import './App.css';
+
+
+const serverUrl = 'http://localhost:3000/';
+const App: react.FC = () => {
+const [message, setMessage] = react.useState<string|undefined>();
+react.useEffect(() => {
+async function fetchHelloWorld() {
+const response = await axios.get<string>(serverUrl);
+setMessage(response.data);
+}
+fetchHelloWorld();
+}, []);
+return (
+<>
+{
+message === undefined
+? <h2>Loading...</h2>
+: <h2>{message}</h2>
+}
+</>
+);
+}
+export default App;
